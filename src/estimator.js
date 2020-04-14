1cd7305b3e1fd98a165913115f44ec9b7ceacbdc
amustapha <amustapha@appzonegroup.com>
2020-04-14 03:18:44 +0100

    Initialization of variables

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,32 @@
+var chain ;
+var estimateCurrentlyInfected;
+var estimateProjectedInfections;
+var estimateSevereCases;
+var estimateBedSpaceAvailability;
+var estimateCasesForICU;
+var estimateCasesForVentilators;
+var estimateDollarsInFlight;
+
 const covid19ImpactEstimator = (data) => {
-    const estimator = chain(
-        //challenge 1
-        estimateCurrentlyInfected,
-        estimateProjectedInfections,
+  const estimator = chain(
+  //challenge 1
+  estimateCurrentlyInfected,
+  estimateProjectedInfections,
 
-        //challenge 2
-        estimateSevereCases,
-        estimateBedSpaceAvailability,
+  //challenge 2
+  estimateSevereCases,
+  estimateBedSpaceAvailability,
 
-        //challenge 3
-        estimateCasesForICU,
-        estimateCasesForVentilators,
-        estimateDollarsInFlight
-    );
-
-
-    return estimator({
-        data,
-        impact: {},
-        severeimpact: {}
+  //challenge 3
+  estimateCasesForICU,
+  estimateCasesForVentilators,
+  estimateDollarsInFlight
+  );    
+  return estimator({
+      data,        
+      impact: {},        
+      severeimpact: {}    
     });
-
 };
 
 export default covid19ImpactEstimator;
