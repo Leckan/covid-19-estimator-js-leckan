e973e4dbe9bd3a3de428b76393240bf74693abc2
amustapha <amustapha@appzonegroup.com>
2020-04-14 03:24:12 +0100

    Format fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,31 +1,29 @@
-var chain ;
-var estimateCurrentlyInfected;
-var estimateProjectedInfections;
-var estimateSevereCases;
-var estimateBedSpaceAvailability;
-var estimateCasesForICU;
-var estimateCasesForVentilators;
-var estimateDollarsInFlight;
+const chain ;
+const estimateCurrentlyInfected;
+const estimateProjectedInfections;
+const estimateSevereCases;
+const estimateBedSpaceAvailability;
+const estimateCasesForICU;
+const estimateCasesForVentilators;
+const estimateDollarsInFlight;
 
 const covid19ImpactEstimator = (data) => {
-  const estimator = chain(
-  //challenge 1
-  estimateCurrentlyInfected,
-  estimateProjectedInfections,
-
-  //challenge 2
-  estimateSevereCases,
-  estimateBedSpaceAvailability,
-
-  //challenge 3
-  estimateCasesForICU,
-  estimateCasesForVentilators,
-  estimateDollarsInFlight
-  );    
-  return estimator({
-      data,        
-      impact: {},        
-      severeimpact: {}    
+    const estimator = chain(
+        // challenge 1  
+        estimateCurrentlyInfected,
+        estimateProjectedInfections,
+        // challenge 2
+        estimateSevereCases,
+        estimateBedSpaceAvailability,
+        //challenge 3
+        estimateCasesForICU,
+        estimateCasesForVentilators,
+        estimateDollarsInFlight);
+        
+    return estimator({
+        data,
+        impact: {},
+        severeimpact: {}
     });
 };
 
