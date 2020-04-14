e4260386f3dbb71bc5728e9cd6004c9183f5e6dd
amustapha <amustapha@appzonegroup.com>
2020-04-14 06:23:19 +0100

    Challenge 2 fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,29 +1,29 @@
 const covid19ImpactEstimator = (data) => {
-  let estimateTime;
+  let estimateDays;
   if (data.periodType === 'days') {
-    estimateTime = data.timeToElapse;
+    estimateDays = data.timeToElapse;
   } else if (data.periodType === 'weeks') {
-    estimateTime = data.timeToElapse * 7;
+    estimateDays = data.timeToElapse * 7;
   } else if (data.periodType === 'months') {
-    estimateTime = data.timeToElapse * 30;
+    estimateDays = data.timeToElapse * 30;
   }
-  const setOfDays = Math.floor(estimateTime / 3);
+  const setOfDays = Math.floor(estimateDays / 3);
   // Challenge 1 constants
   const infected = data.reportedCases * 10;
   const periodInfected = infected * (2 ** setOfDays);
   const severeInfected = data.reportedCases * 50;
   const severePeriodInfected = severeInfected * (2 ** setOfDays);
   // Challenge 2 constants
-  const periodCases = Math.floor((15 / 100) * periodInfected);
-  const severePeriodCases = Math.floor((15 / 100) * severePeriodInfected);
-  const beds = Math.floor(((35 / 100) * data.totalHospitalBeds) - periodCases);
-  const severeBeds = Math.floor(((35 / 100) * data.totalHospitalBeds) - severePeriodCases);
+  const periodCases = Math.floor(periodInfected * (15 / 100));
+  const severePeriodCases = Math.floor(severePeriodInfected * (15 / 100));
+  const beds = Math.floor((data.totalHospitalBeds * (35 / 100)) - periodCases);
+  const severeBeds = Math.floor((data.totalHospitalBeds * (35 / 100)) - severePeriodCases);
   // Challenge 3 constants
 
   const impact = {
     currentlyInfected: infected,
     infectionsByRequestedTime: periodInfected,
-    severeCasesByrequestedTime: periodCases,
+    severeCasesByRequestedTime: periodCases,
     hospitalBedsByRequestedTime: beds,
     casesForICUByRequestedTime: 0,
     casesForVentilatorsByRequestedTime: 0,
@@ -32,7 +32,7 @@ const covid19ImpactEstimator = (data) => {
   const severeImpact = {
     currentlyInfected: severeInfected,
     infectionsByRequestedTime: severePeriodInfected,
-    severeCasesByrequestedTime: severePeriodCases,
+    severeCasesByRequestedTime: severePeriodCases,
     hospitalBedsByRequestedTime: severeBeds,
     casesForICUByRequestedTime: 0,
     casesForVentilatorsByRequestedTime: 0,
