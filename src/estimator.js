4a00ad41402e7db8ac6c501ad8c247a40f3c7206
amustapha <amustapha@appzonegroup.com>
2020-04-14 02:57:55 +0100

    initialization

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,12 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return{
+        data:input,
+        impact: {},
+        severeimpact: {}
+
+    }
+
+};
 
 export default covid19ImpactEstimator;
