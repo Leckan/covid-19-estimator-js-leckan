01da9f993958b172e56f704a2fa7a7bb58b8e995
amustapha <amustapha@appzonegroup.com>
2020-04-14 03:39:26 +0100

    Format fixes

--- src/estimator.js
+++ src/estimator.js
@@ -1,30 +1,31 @@
-const chain = data;
-const estimateCurrentlyInfected = data;
-const estimateProjectedInfections = data;
-const estimateSevereCases = data;
-const estimateBedSpaceAvailability = data;
-const estimateCasesForICU = data;
-const estimateCasesForVentilators = data;
-const estimateDollarsInFlight = data;
+const chain = ({});
+const estimateCurrentlyInfected = {};
+const estimateProjectedInfections = {};
+const estimateSevereCases = {};
+const estimateBedSpaceAvailability = {};
+const estimateCasesForICU = {};
+const estimateCasesForVentilators = {};
+const estimateDollarsInFlight = {};
 
 const covid19ImpactEstimator = (data) => {
-    const estimator = chain(
-        // challenge 1  
-        estimateCurrentlyInfected,
-        estimateProjectedInfections,
-        // challenge 2
-        estimateSevereCases,
-        estimateBedSpaceAvailability,
-        //challenge 3
-        estimateCasesForICU,
-        estimateCasesForVentilators,
-        estimateDollarsInFlight);
+  const estimator = chain(
+    // challenge 1  
+    estimateCurrentlyInfected,
+    estimateProjectedInfections,
+    // challenge 2
+    estimateSevereCases,
+    estimateBedSpaceAvailability,
+    // challenge 3
+    estimateCasesForICU,
+    estimateCasesForVentilators,
+    estimateDollarsInFlight
+    );
         
-    return estimator({
-        data,
-        impact: {},
-        severeimpact: {}
-    });
+  return estimator({
+    data,
+    impact: {},
+    severeimpact: {}
+  });
 };
 
 export default covid19ImpactEstimator;
